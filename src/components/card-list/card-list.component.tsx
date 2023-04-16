8bae56ed8d6f01b28fee22997cb5327d0c4df7ee
Targa82 <stanojevic.milosh@gmail.com>
2023-04-16 11:14:15 +0200

    Initial TypeScript Version

---
+++ src/components/card-list/card-list.component.tsx
@@ -0,0 +1,17 @@
+import Card from "../card/card.component";
+import { Monster } from "../../App";
+import "./card-list.styles.css";
+
+type CardListProp = {
+  monsters: Monster[];
+};
+
+const CardList = ({ monsters }: CardListProp) => (
+  <div className="card-list">
+    {monsters.map((monster) => {
+      return <Card key={monster.id} monster={monster} />;
+    })}
+  </div>
+);
+
+export default CardList;
